cea2509d2af990289eccfff86b1c07933db116e2
jorge luis <martinezmonajorgeluis@gmail.com>
2023-06-13 07:54:44 -0500

    This is the commit correspondent to de class number 9 of cours of ECMAScript by Platzi.

---
+++ src/es6/02-strings.js
@@ -0,0 +1,15 @@
+let hello = "Hello"
+let world = "world"
+let epicPhrase = hello + ' ' + world + '!'
+console.log(epicPhrase)
+
+// template literal
+
+let epicPhrase2 = `${hello} ${world}!` 
+
+// multi-line strings
+
+let lorem = 'esto es un estring \n' + 'esto es otro estring'
+
+let lorem = ` esto es una frase epica 
+esto es la continuacion de esa frase epica`
