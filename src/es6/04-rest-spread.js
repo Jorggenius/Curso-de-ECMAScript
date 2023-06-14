42c77b94eebcd69895a1504f5a6853d3d64c55da
jorge luis <martinezmonajorgeluis@gmail.com>
2023-06-14 08:37:32 -0500

    In this commit we saw the code to destructure the contend of an array or an object this code is of the clas number 7 of cours of ECMAScript by Platzi.

---
+++ src/es6/04-rest-spread.js
@@ -0,0 +1,11 @@
+// arrays destructuring
+
+let fruits = ['apple', 'Banana']
+let [a, b] = fruits
+console.log(a, b)
+
+// object destructuring
+
+let user = {username: 'oscar', age: 34}
+let {username, age} = user
+console.log(username, age)
